dbd8de3d5861830dbc7ed0f2ec6d6557db20f636
Timothee Thomance <timotheet11@gmail.com>
2025-04-23 18:36:51 -0400

    getRange complete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @returns {number} the length of the array
  */
 function getLength(numbers) {
-  // TODO
+  return numbers.length;
 }
 
 /**
@@ -11,7 +11,7 @@ function getLength(numbers) {
  * @returns {number} the sum of the numbers
  */
 function getSum(numbers) {
-  // TODO
+  return numbers.reduce((total, val) => {return total + val}, 0);
 }
 
 /**
@@ -19,7 +19,7 @@ function getSum(numbers) {
  * @returns {number} the mean of the numbers
  */
 function getMean(numbers) {
-  // TODO
+  return getSum(numbers) / getLength(numbers);
 }
 
 /**
@@ -27,7 +27,13 @@ function getMean(numbers) {
  * @returns {number} the smallest of the numbers
  */
 function getMin(numbers) {
-  // TODO
+  let minValue = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < minValue) {
+      minValue = numbers[i];
+    }
+  }
+  return minValue;
 }
 
 /**
@@ -35,7 +41,13 @@ function getMin(numbers) {
  * @returns {number} the largest of the numbers
  */
 function getMax(numbers) {
-  // TODO
+  let maxValue = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > maxValue) {
+      maxValue = numbers[i];
+    }
+  }
+  return maxValue;
 }
 
 /**
@@ -43,7 +55,7 @@ function getMax(numbers) {
  * @returns {number} the range of the numbers (max - min)
  */
 function getRange(numbers) {
-  // TODO
+  return getMax(numbers) - getMin(numbers);
 }
 
 /**
@@ -93,8 +105,8 @@ function describeNumbers(numbers) {
   console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
   console.log(`The largest of your numbers is ${getMax(numbers)}.`);
   console.log(`The range of your numbers is ${getRange(numbers)}.`);
-  console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
-  console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
+  // console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
+  // console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
 }
 
 const userInputString = prompt(
